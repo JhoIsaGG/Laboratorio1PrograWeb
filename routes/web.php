959e661823e7b782e629aa06c36b7906b302c033
JhoIsaGG <jhonatangaliano123@gmail.com>
2026-08-20 21:06:48 -0600

    Cambio de orden en middleware

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AcademicController;
 use App\Http\Middleware\TraceRequest;
-use App\Http\Middleware\RequireClientKey;
 use App\Http\Middleware\MeasureResponseTime;
+use App\Http\Middleware\RequireClientKey;
 
 Route::get('/academic/courses', [
     AcademicController::class,
     'courses'
 ])->middleware([
+    MeasureResponseTime::class,
     TraceRequest::class,
     RequireClientKey::class,
-    MeasureResponseTime::class,
 ]);
